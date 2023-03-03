492cc13a4f63a8a74a612267dd5a64b0049faa09
hyihua <hyihua@wayne.edu>
2023-03-02 23:03:11 -0500

    update scheduler event and bigSponsor query

--- contest_status_update.sql
+++ contest_status_update.sql
@@ -3,13 +3,12 @@ use contestdb;
 -- update status for a contest --
 drop procedure if exists contest_start_status;
 drop procedure if exists contest_close_status;
-drop procedure if exists contest_past_status;
 
 DELIMITER $$
 CREATE DEFINER=`john`@`%` PROCEDURE `contest_start_status`()
 BEGIN
 	update contest set status = 'opened'
-    where status = 'created' and (UNIX_TIMESTAMP(begin_time) >= unix_timestamp(now()));
+    where status = 'created' and (UNIX_TIMESTAMP(begin_time) <= unix_timestamp(now()));
 END$$
 DELIMITER ;
 
@@ -17,17 +16,20 @@ DELIMITER $$
 CREATE DEFINER=`john`@`%` PROCEDURE `contest_close_status`()
 BEGIN
 	update contest set status = 'closed'
-    where status = 'opened' and (UNIX_TIMESTAMP(end_time) = unix_timestamp(now()));
+    where status = 'opened' and (UNIX_TIMESTAMP(end_time) <= unix_timestamp(now()));
 END$$
 DELIMITER ;
 
-DELIMITER $$
-CREATE DEFINER=`john`@`%` PROCEDURE `contest_past_status`()
-BEGIN
-	update contest set status = 'past'
-    where status = 'closed' and (UNIX_TIMESTAMP(end_time) < unix_timestamp(now()));
-END$$
-DELIMITER ;
+-- DELIMITER $$
+-- CREATE DEFINER=`john`@`%` PROCEDURE `contest_past_status`()
+-- BEGIN
+-- 	update contest set status = 'past'
+--     where status = 'closed' and (UNIX_TIMESTAMP(end_time) < unix_timestamp(now()));
+-- END$$
+-- DELIMITER ;
+
+-- set scheduled task on --
+set global event_scheduler = on;
 
 drop event if exists check_contest_status;
 delimiter |
@@ -37,12 +39,13 @@ every '10' second starts '2023-03-02 08:00:00'
 comment 'check if contest stauts needs to be updated'
 do
 	begin
-		CALL `contestdb`.`contest_close_status`();
-		CALL `contestdb`.`contest_past_status`();
 		CALL `contestdb`.`contest_start_status`();
+		CALL `contestdb`.`contest_close_status`();
 	end |
     
 delimiter ; 
+
+show variables like 'event%';
 
 
 
--- root_user_functionalities.sql
+++ root_user_functionalities.sql
@@ -1,7 +1,8 @@
 use contestdb;
 
 -- Big Sponsors --
-select sponsor_id
+select * from sponsor where sponsor_id in (
+select sponsor_id as selected_sponsor
 from contest group by sponsor_id
 having count(sponsor_id)=(
 	select max(count_num)
@@ -9,4 +10,4 @@ having count(sponsor_id)=(
 		select sponsor_id, count(sponsor_id) count_num
 		from contest
 		group by sponsor_id) groupby_sponsor 
-);
+) ) ;
